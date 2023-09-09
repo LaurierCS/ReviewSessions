99bf673b6b33514f841d1b4f81d5671a84b417d7
Steven Tohme <steventohme59@gmail.com>
2023-09-09 16:16:14 -0400

    add review session 1 content

---
+++ CP104/ReviewSession1/ReviewSession1Content.py
@@ -0,0 +1,45 @@
+# 1. Data Types
+
+# Integer
+int1 = 10
+int2 = -15
+
+# Float
+float1 = 3.14
+float2 = 0.0
+
+# String
+string1 = "Hello World"
+string2 = 'I love CP104'
+
+# Boolean
+bool1 = True
+bool2 = False
+
+# 2. Console Output 
+print("Hello World")
+string1 = "Hello World"
+print(string1)
+
+print("Hello World" + string1)
+
+print()
+
+# 3. Formatted Output
+name = "Steven"
+formattedString = f"Hello World! My name is {name}"
+
+# Formatting other data types
+int1 = 10
+float1 = 3.14
+bool1 = True
+
+formattedInt = f"Integer: {int1:d}"
+formattedFloat = f"Float: {float1:f}"
+formattedBool = f"Boolean: {bool1:b}"
+
+# Rounding floats
+formattedFloat = f"Float: {float1:.1f}"
+
+# Spacing
+formattedInt = f"Integer: {int1:5d}"
