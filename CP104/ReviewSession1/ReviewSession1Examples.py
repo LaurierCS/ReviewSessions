6aa05e0b6429f0a419eff175ae5817ce51216f99
Steven Tohme <steventohme59@gmail.com>
2023-09-09 19:14:08 -0400

    add second example

--- CP104/ReviewSession1/ReviewSession1Examples.py
+++ CP104/ReviewSession1/ReviewSession1Examples.py
@@ -1,3 +1,18 @@
 # 1.  print the following using an f string: |    12345| |     123.45| |        Hi There|
 print(f"|{12345:>10d}| |{123.45:>10.2f}| |{'Hi There':>10s}|")
 
+# 2.  write a docstring for the following function: 
+def calcDistance(speed, time):
+    return speed * time
+
+def calcDistance(speed, time):
+    """Calculates the distance travelled given the speed and time travelled
+
+    Args:
+        speed (float): speed travelled in km/h
+        time (float): time travelled in hours
+
+    Returns:
+        float: distance travelled in km
+    """
+    return speed * time
