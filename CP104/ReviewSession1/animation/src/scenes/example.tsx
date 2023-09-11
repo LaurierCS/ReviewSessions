58b9ccab67da8e4900e8274d1f66c11e11ffd571
Steven Tohme <steventohme59@gmail.com>
2023-09-11 15:26:04 -0400

    add first codeblock to animation

--- CP104/ReviewSession1/animation/src/scenes/example.tsx
+++ CP104/ReviewSession1/animation/src/scenes/example.tsx
@@ -1,8 +1,29 @@
-import {makeScene2D} from '@motion-canvas/2d';
-import {waitFor} from '@motion-canvas/core';
+import {makeScene2D, Txt} from '@motion-canvas/2d';
+import {beginSlide, waitFor, createRef} from '@motion-canvas/core';
+import {CodeBlock, lines} from '@motion-canvas/2d/lib/components/CodeBlock';
 
 export default makeScene2D(function* (view) {
   // Create your animations here
+
+  const codeRef = createRef<CodeBlock>();
   
-  yield* waitFor(5);
-});
+  yield* beginSlide('first slide');
+  yield view.add(
+    <>
+      <CodeBlock
+        language='python'
+        ref={codeRef}
+        fontSize={30}
+        alignItems={'center'}
+
+        code={() => `int1 = 10
+int2 = -15`}
+      /> 
+    </>
+  );
+
+  yield * codeRef().selection(lines(0), 1);
+  yield * waitFor(5);
+  yield * codeRef().selection(lines(1), 1);
+  yield * waitFor(5);
+  });
