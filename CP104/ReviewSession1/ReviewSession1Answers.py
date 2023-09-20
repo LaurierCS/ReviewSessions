f7edfd68fa0e546e1634860fef1490685c3d6ce6
Steven Tohme <steventohme59@gmail.com>
2023-09-19 20:14:40 -0400

    udpate examples

--- CP104/ReviewSession1/ReviewSession1Answers.py
+++ CP104/ReviewSession1/ReviewSession1Answers.py
@@ -1,7 +1,14 @@
 # 1. 
 print(f"|{12345:>10d}| |{123.45:>10.2f}| |{'Hi There':>10s}|")
 
+# 2. 
+left = "left"
+middle = "middle"
+right = "right"
 
+print(f"{left:*>10}")
+print(f"{middle:*^10}")
+print(f"{right:*<10}")
 
 # 3. 
 def formatTime(hour, minute):
--- CP104/ReviewSession1/ReviewSession1Examples.py
+++ CP104/ReviewSession1/ReviewSession1Examples.py
@@ -1,7 +1,10 @@
-# 1.  print the following using an f string: |    12345| |     123.45| |        Hi There|
+# 1.  print the following using an f string: |    12345| |    123.456| |        Hi There|
 
 
-
+# 2. align the following words as follows (each line has 10 characters): 
+# left******
+# **middle**
+# *****right
 
 # 3. write a function that takes an hour and minute and returns a formatted analog clock time
 #    e.g. 1, 30 -> 01:30
