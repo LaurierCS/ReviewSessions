cff4d4aeb156ea00bfc12ab1f4b3da39742998cc
Steven Tohme <steventohme59@gmail.com>
2023-10-03 22:13:01 -0400

    add second practice problem

--- CP104/ReviewSession2/ReviewSession2Answers.py
+++ CP104/ReviewSession2/ReviewSession2Answers.py
@@ -11,3 +11,26 @@ def calcDistance(speed, time):
         float: distance travelled in km
     """
     return speed * time
+
+# 2. 
+def add_fraction(num1, den1, num2, den2):
+    """
+    -------------------------------------------------------
+    Calculates and returns fraction values.
+    Use: num, den, product = multiply_fractions(num1, den1, num2, den2)
+    -------------------------------------------------------
+    Parameters:
+        num1 - numerator of first fraction (int)
+        den1 - denominator of first fraction (int != 0)
+        num2 - numerator of second fraction (int)
+        den2 - denominator of second fraction (int != 0)
+    Returns:
+        num - numerator of sum (int)
+        den - denominator of sum (int)
+        sum - num / den (float)
+    --------
+    """
+    num = num1 * den2 + num2 * den1
+    den = den1 * den2
+    sum = num / den
+    return num, den, sum
--- CP104/ReviewSession2/ReviewSession2Examples.py
+++ CP104/ReviewSession2/ReviewSession2Examples.py
@@ -1,3 +1,23 @@
 # 1.  write a docstring for the following function: 
 def calcDistance(speed, time):
     return speed * time
+
+# 2. implement this function
+def add_fraction(num1, den1, num2, den2):
+    """
+    -------------------------------------------------------
+    Calculates and returns fraction values.
+    Use: num, den, product = multiply_fractions(num1, den1, num2, den2)
+    -------------------------------------------------------
+    Parameters:
+        num1 - numerator of first fraction (int)
+        den1 - denominator of first fraction (int != 0)
+        num2 - numerator of second fraction (int)
+        den2 - denominator of second fraction (int != 0)
+    Returns:
+        num - numerator of sum (int)
+        den - denominator of sum (int)
+        sum - num / den (float)
+    --------
+    """
+    
