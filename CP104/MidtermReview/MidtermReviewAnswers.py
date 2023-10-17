5abedb853891d1e003aa2dee05b561a360c85923
Steven Tohme <steventohme59@gmail.com>
2023-10-17 15:03:21 -0400

    add second example

--- CP104/MidtermReview/MidtermReviewAnswers.py
+++ CP104/MidtermReview/MidtermReviewAnswers.py
@@ -1,4 +1,20 @@
-#1. 
+# 1. 
+def number_staircase(start, end, increment):
+    """
+    prints numbers in descending order from start to end by increment
+    Use: number_staircase(start, end, increment)
+
+    Parameters:
+        start (int) - starting number
+        end (int) - ending number
+        increment (int) - number to increment by
+
+        start > end
+    """
+    for i in range(start, end, -increment):
+        print(i)
+
+# 2. 
 def calculate_factorial(target):
     """
     calculates the factorial of a number (target!)
@@ -14,3 +30,4 @@ def calculate_factorial(target):
     for i in range(1, target):
         factorial *= i
     return factorial
+
--- CP104/MidtermReview/MidtermReviewExamples.py
+++ CP104/MidtermReview/MidtermReviewExamples.py
@@ -1,5 +1,18 @@
-# 1. Complete the docstring and implement the following function
+# 1. Implement the following function
+def number_staircase(start, end, increment):
+    """
+    prints numbers in descending order from start to end by increment
+    Use: number_staircase(start, end, increment)
+
+    Parameters:
+        start (int) - starting number
+        end (int) - ending number
+        increment (int) - number to increment by
+
+        start > end
+    """
+
+# 2. Complete the docstring and implement the following function
 def calculate_factorial(target):
     """
     """
-
