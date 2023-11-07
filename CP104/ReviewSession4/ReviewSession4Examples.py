dd8f66d36d5ba807c5585453b97d984fe351802e
Steven Tohme <steventohme59@gmail.com>
2023-11-07 15:38:01 -0500

    add example 1

---
+++ CP104/ReviewSession4/ReviewSession4Examples.py
@@ -0,0 +1,17 @@
+# 1. Implement the following function
+def union(source1, source2):
+    """
+    -------------------------------------------------------
+    Returns a string that is the union of the contents of source1 and source2.
+    Every element that appears at least once in either source1 and source2
+    must appear once and only once in target.
+    Use: target = union(source1, source2)
+    -------------------------------------------------------
+    Parameters:
+        source1 (str) - a string
+        source2 (str) - a string
+    Returns:
+        target (str) - the union of source1 and source2
+    -------------------------------------------------------
+    """
+    
