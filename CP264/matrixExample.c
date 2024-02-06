0557978c03ee1111d578c07baf45fda169e41043
noahshaffir <noahshaffir@gmail.com>
2024-02-05 20:01:50 -0500

    updated matrix example

--- CP264/matrixExample.c
+++ CP264/matrixExample.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
+    int matrix[3][3]=
+    {{1,2,3},
+    {4,5,6},
+    {7,8,9}};
+    for(int i=0;i<3;i++){
+        for(int j=0; j<3; j++){
+            printf("%2d ", matrix[i][j]); //print value of element in the matrix
+        }
+        printf("\n"); //print newline character
+    }
     return 0;
 }
